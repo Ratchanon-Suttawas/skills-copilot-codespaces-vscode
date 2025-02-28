1a3b8973bd11cb0c933e26a60ce49d1b50be9e82
Ratchanon-Suttawas <ratchanonsuttawas@outlook.co.th>
2025-02-28 22:50:41 +0000

    Enhance web server setup with Express and file upload functionality

--- comment.js
+++ comment.js
@@ -1,1 +1,51 @@
-// Create web server
+// Create web server with express
+
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var cookieParser = require('cookie-parser');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).array('image'));
+app.use(cookieParser());
+
+app.get('/index.html', function (req, res) {
+    res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.post('/file_upload', function (req, res) {
+    console.log(req.files[0]);  // 上传的文件信息
+
+    var des_file = __dirname + '/' + req.files[0].originalname;
+    fs.readFile(req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                response = {
+                    message: 'File uploaded successfully',
+                    filename: req.files[0].originalname
+                };
+            }
+            console.log(response);
+            res.end(JSON.stringify(response));
+        });
+    });
+});
+
+app.get('/', function (req, res) {
+    console.log('Cookies: ', req.cookies);
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('访问地址为 http://%s:%s', host, port);
+});
+
+
+
